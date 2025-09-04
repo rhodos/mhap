6d815e75ab38bdc3126687cdaad028d1d79adb2f
Rachel Hodos-Nkhereanye <rachelhodos@gmail.com>
2025-09-04 12:48:13 -0400

    Added script to perform app classification with gpt model.

---
+++ code/dataset_construction/app_classification_gpt.py
@@ -0,0 +1,126 @@
+import pandas as pd
+import numpy as np
+import os
+
+from tqdm.auto import tqdm
+from datetime import datetime
+
+from openai import OpenAI
+
+gpt_client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+model = "gpt-4.1-mini"
+
+
+def get_first_k_lines(text, k):
+    lines = text.split("\n")
+    return " ".join(lines[:k])
+
+
+def construct_prompt(data_point):
+    title = data_point["title"]
+    description = get_first_k_lines(data_point["description"], k=4)
+    preamble = "Classify the following app as 'Mental Health', 'Not Mental Health', or 'Unclear/Borderline' based on its title and the first few lines of its description. Apps with journals or mood trackers can be considered Mental Health. A sleep support app that just focuses on white noise would not be considered Mental Health, but if it includes guided meditations or mantras, it would. Just output the classification without any further discussion."
+    prompt = f"""{preamble}
+
+    Title: {title}
+    Description: {description}
+    """
+    return prompt
+
+
+def prompt_model(prompt, model, verbose=True):
+    if verbose:
+        print("PROMPT:\n", prompt)
+    response = gpt_client.responses.create(model=model, input=prompt)
+    if verbose:
+        print("MODEL:\n", response.output_text)
+    return response.output_text
+
+
+def get_data_point(data, index):
+    return dict(data.iloc[index])
+
+
+def generate_metrics_and_predictions(data, verbose=True):
+    predictions = pd.DataFrame(columns=["prediction", "label"])
+    for i in range(len(data)):
+        if verbose:
+            print("DATA POINT ", i)
+        data_point = get_data_point(data, i)
+        label = data_point["label"]
+        prompt = construct_prompt(data_point)
+        output = prompt_model(prompt, model, verbose=False).strip()
+        if verbose:
+            print("OUTPUT:", output)
+        if verbose:
+            print("TRUE LABEL:", label)
+        predictions.loc[i] = [output, label]
+        if verbose:
+            print("\n\n")
+    predictions["correct"] = predictions["prediction"] == predictions["label"]
+    accuracy = predictions[predictions.label != "Unclear/Borderline"].correct.mean()
+    num_uncertain = predictions[predictions.label == "Unclear/Borderline"].shape[0]
+    percent_uncertain = 100.0 * num_uncertain / predictions.shape[0]
+    if verbose:
+        print("ACCURACY:", accuracy)
+    if verbose:
+        print("PERCENT UNCERTAIN:", percent_uncertain)
+    metrics = {"accuracy": accuracy, "percent_uncertain": percent_uncertain}
+    return metrics, predictions
+
+
+def label_data(data):
+    if "prediction" not in data.columns:
+        data["prediction"] = None
+
+    data = data.reset_index(drop=True)
+
+    for i in tqdm(range(len(data))):
+        data_point = get_data_point(data, i)
+        prompt = construct_prompt(data_point)
+        output = prompt_model(prompt, model, verbose=False).strip()
+        data.loc[i, "prediction"] = output
+
+    return data
+
+
+if __name__ == "__main__":
+
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    data_dir = script_dir + "/../../data/step3_llm_annotations/"
+
+    val_data = pd.read_csv(data_dir + "validation_data.tsv", sep="\t")
+    val_metrics, val_predictions = generate_metrics_and_predictions(
+        val_data, verbose=True
+    )
+    print(val_metrics)
+
+    data = pd.read_csv(data_dir + "all_data_manual_labels.tsv", sep="\t")
+    data.rename(
+        columns={
+            "Mental Health": "label",
+            "Name": "title",
+            "Description": "description",
+            "Source": "source",
+        },
+        inplace=True,
+    )
+
+    # sample_data = data.sample(10)
+    # labeled_sample = label_data(sample_data)
+    # columns= ['title', 'description', 'label', 'prediction']
+    # labeled_sample[columns]
+
+    labeled_data = label_data(data)
+
+    manual_labels = labeled_data[
+        labeled_data.label.isin(["Mental Health", "Not Mental Health"])
+    ]
+    accuracy_on_all_labels = np.mean(manual_labels.label == manual_labels.prediction)
+    print("Accuracy on all labels:", accuracy_on_all_labels)
+
+    labeled_data["ID"] = range(1, len(labeled_data) + 1)
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    output_filename = f"labeled_data_{timestamp}.tsv"
+    labeled_data.to_csv(data_dir + output_filename, sep="\t", header=True, index=False)
